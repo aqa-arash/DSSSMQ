b34beb6a49dd1bb4781de30e9479925217feb47f
Arash Moradian <moradian.arash@gmail.com>
2024-10-30 19:02:51 +0100

    packaging

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+# This file makes this directory a package.
+
+# You can import your modules here if you want to make them available
+# at the package level, like this:
+# from .math_quiz import Random_Integer, Operator_Selector, Solver, math_quiz
+
+# Or you can leave it empty if you don't need to do any package-level imports.
