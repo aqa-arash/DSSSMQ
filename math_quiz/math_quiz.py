d8ad3066cf49a705af8d36814fc0b4b8a91bc7c9
Arash Moradian <moradian.arash@gmail.com>
2024-10-30 18:42:59 +0100

    Bug fixes and improved readability

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,99 @@
 import random
 
 
-def function_A(min, max):
+def Random_Integer(min, max):
     """
-    Random integer.
+    Generate a random integer between min and max.
+
+    Args:
+        min: Lower limit of the random integer.
+        max: Upper limit of the random integer.
+
+    Returns:
+        random integer between min and max.
+
+    Raises:
+        KeyError: See random.randint error handling.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def Operator_Selector():
+    """
+    Generate a random operator to be used in the quiz.
+
+    Returns:
+        random operator as a string.
+
+    Raises:
+        KeyError: no error expected.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def Solver(n1, n2, o):
+    """
+    Calculates the result of the randomly generated quiz problem
+
+    Args:
+        n1: First random integer.
+        n2: Second random integer.
+        o: Operator
+
+    Returns:
+        resulting integer.
+
+    Raises:
+        KeyError: See random.randint error handling.
+    """
+
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    The main code managing the quiz. Generates random math operation and solution, prompts the user for input and compares and keeps score for 10 rounds.
+
+    Function calls:
+    	Random_Integer 
+    	Operator_Selector
+    	Solver
+    	
+    Returns:
+        Interactive. 
+
+    Raises:
+        ValueError: If a non integer input is given by user.
+    """
+
+    Correct_Answers = 0
+    Num_Questions = 10
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(Num_Questions):
+        n1 = Random_Integer(1, 10); n2 = Random_Integer(1, 6); o = Operator_Selector()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Solver(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        
+        try:
+            useranswer = int(useranswer)
+        except:
+            print("That was not an integer")
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            Correct_Answers += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {Correct_Answers}/{Num_Questions}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,38 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Random_Integer, Operator_Selector, Solver
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_Random_Integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Random_Integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_Operator_Selector(self):
+        # test if the output is within range of expected values
+        for _ in range(1000):  # Test a large number of random values
+            random_operator = Operator_Selector()
+            self.assertTrue(random_operator=='+' or random_operator=='*' or random_operator=='-')
 
-    def test_function_C(self):
+    def test_Solver(self):
+            # test the solver for some problems, feel free to add more
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 0, '+', '5 + 0', 5),
+                (5, 2, '-', '5 - 2', 3),
+                (5, 0, '-', '5 - 0', 5),
+                (5, 2, '*', '5 * 2', 10),
+                (5, 0, '*', '5 * 0', 0)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                PROBLEM, ANSWER = Solver(num1, num2, operator)
+                self.assertTrue(PROBLEM==expected_problem and ANSWER==expected_answer)
+                
 
 if __name__ == "__main__":
     unittest.main()
